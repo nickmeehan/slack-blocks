9113270db90e3152d617e292b33d90a16c26d13a
Nick Meehan <nnmeehan@gmail.com>
2022-04-07 08:25:55 -0700

    Initial Commit

---
+++ lib/slack_blocks/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SlackBlocks
+  VERSION = "0.1.0"
+end
---
+++ spec/slack_blocks_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SlackBlocks do
+  it "has a version number" do
+    expect(SlackBlocks::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
