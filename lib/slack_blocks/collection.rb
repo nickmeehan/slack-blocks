630be5167d6225d46c4fb6f6fc3551b5468a1f90
Nick Meehan <nnmeehan@gmail.com>
2022-05-08 09:40:17 -0700

    add validation for blocks array size and top-level block types

--- lib/slack_blocks/collection.rb
+++ lib/slack_blocks/collection.rb
@@ -2,20 +2,38 @@
 
 require 'json'
 
+require_relative 'divider'
+require_relative 'header'
+
 module SlackBlocks
   class Collection
     MAX_ELEMENTS_SIZE = 50
+    VALID_BLOCK_KLASSES = [
+      # actions,
+      # conditions,
+      # context,
+      SlackBlocks::Divider,
+      # image,
+      # section,
+      SlackBlocks::Header,
+      # table,
+      # contact_card,
+      # share_shortcut,
+      # share_workflow,
+      # video
+    ].to_set
+
+    # Cache this so we don't ever have to compute this on the fly.
+    VALID_BLOCK_KLASS_STRINGS = VALID_BLOCK_KLASSES.join(', ')
 
     def initialize
       @blocks = []
     end
 
-    def <<(block)
-      # We check the current size to see if the @blocks array can fits anymore.
-      if @blocks.size >= MAX_ELEMENTS_SIZE
-        raise SlackBlocks::TooManyElements, "the maximum number of blocks to send at one time is #{MAX_ELEMENTS_SIZE}"
-      end
-      @blocks << block
+    def <<(incoming_block)
+      validate_block_klass(incoming_block)
+      validate_block_addition
+      @blocks << incoming_block
     end
 
     def as_json
@@ -24,6 +42,25 @@ module SlackBlocks
 
     def to_json
       JSON.unparse(as_json)
+    end
+
+    private
+
+    def validate_block_klass(incoming_block)
+      # We check the incoming block's class to ensure it's valid.
+      unless VALID_BLOCK_KLASSES.include?(incoming_block.class)
+        raise SlackBlocks::InvalidBlockType,
+          "#{incoming_block.class} is not a valid top-level block type, valid block types are #{VALID_BLOCK_KLASS_STRINGS}"
+      end
+      if @blocks.size >= MAX_ELEMENTS_SIZE
+      end
+    end
+
+    def validate_block_addition
+      # We check the current size to see if the @blocks array can fits anymore.
+      if @blocks.size >= MAX_ELEMENTS_SIZE
+        raise SlackBlocks::TooManyElements, "the maximum number of blocks to send at one time is #{MAX_ELEMENTS_SIZE}"
+      end
     end
   end
 end
