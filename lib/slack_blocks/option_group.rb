e0cec3d310e9095f0679343097b3a96df76f93a4
Nick Meehan <nnmeehan@gmail.com>
2022-05-21 13:12:47 -0700

    support option group composition element

---
+++ lib/slack_blocks/option_group.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require_relative 'option'
+
+# Supports the OptionGroup composition object.
+#
+# @see https://api.slack.com/reference/block-kit/composition-objects#option_group
+module SlackBlocks
+  class OptionGroup
+    include Collectable
+
+    max_collection_size(100)
+    valid_block_klasses(SlackBlocks::Option)
+    collection_instance_variable_name('@options')
+
+    def initialize(label:, options: [])
+      @label =
+        if label.is_a?(String)
+          SlackBlocks::PlainText.new(text: label)
+        elsif label.is_a?(SlackBlocks::PlainText)
+          label
+        else
+          fail ArgumentError, 'must pass a String or SlackBlocks::PlainText to label keyword argument'
+        end
+      @options = options
+      validate_collection_size
+      validate_collection_contents
+    end
+
+    def as_json
+      {
+        'label' => @label.as_json,
+        'options' => @options.map(&:as_json)
+      }.compact
+    end
+  end
+end
